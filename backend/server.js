052957d20771dcbc5b2fffe682703a2c8cecbe1f
Anar Seferov <anarsferov@gmail.com>
2022-09-22 11:50:02 +0400

    update added

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ const app = express();
 const { v4: uuidv4 } = require("uuid");
 
 let data = [
-  { id: 1, data: { name: "iPhone", price: 1000, costPrice: 400, img: "" } },
-  { id: 2, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
-  { id: 3, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
-  { id: 4, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
-  { id: 5, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
-  { id: 6, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
+    { id: 1, data: { name: "iPhone", price: 1000, costPrice: 400, img: "" } },
+    { id: 2, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
+    { id: 3, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
+    { id: 4, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
+    { id: 5, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
+    { id: 6, data: { name: "Samsung", price: 800, constPrice: 300, img: "" } },
 ];
 
 app.use(cors());
@@ -21,17 +21,17 @@ app.use(express.json());
 app.use("/public", express.static(path.resolve("assets")));
 
 app.get("/", (req, res) => {
-  res.send(JSON.stringify(data));
+    res.send(JSON.stringify(data));
 });
 
 app.post("/post", (req, res) => {
-  const schema = {
-    id: uuidv4(),
-    data: req.body,
-  };
-  data.push(schema);
-  console.log(schema);
-  res.send(data);
+    const schema = {
+        id: uuidv4(),
+        data: req.body,
+    };
+    data.push(schema);
+    console.log(schema);
+    res.send(data);
 });
 
 app.delete('/delete/:id', (req, res) => {
@@ -42,12 +42,12 @@ app.delete('/delete/:id', (req, res) => {
     res.send(data)
 })
 
-// app.put('/update/:id', (req, res) => {
-//     const ID = req.params.id;
-//     data = data.filter(data => data.id !== ID)
-//     res.send(data)
-// })
+app.put('/update/:id', (req, res) => {
+    const ID = req.params.id;
+    data = data.filter(dat => dat.id === ID ? dat.data = req.body : dat)
+    res.send(data)
+})
 
 app.listen(8080, () => {
-  console.log("Server running on 8080");
+    console.log("Server running on 8080");
 });
