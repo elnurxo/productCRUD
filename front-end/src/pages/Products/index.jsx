8873ba80e56e7b7f2846273a68990c56b793bac2
hajarabdullayeva <hajar.pa@code.local>
2022-09-22 10:13:03 +0400

    fixed

---
+++ front-end/src/pages/Products/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddProducts from "../../components/AddProduct";
+import ProductTable from "../../components/ProductTable";
+
+const Products = () => {
+  return (
+    <>
+      <AddProducts />
+      <ProductTable />
+    </>
+  );
+};
+
+export default Products;
