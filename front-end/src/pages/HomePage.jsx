3dba34ac8bd4aff56d9ed4fecc82b481757d7c1b
elnurxo <elnurykh@code.edu.az>
2022-09-22 09:56:07 +0400

    folder structure upgrade

---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SupplierTable from '../components/SupplierTable'
+
+function HomePage() {
+  return (
+    <>
+    <SupplierTable/>
+    </>
+  )
+}
+
+export default HomePage
