31161e5009b570269d677babee994e2475641906
hajarabdullayeva <hajar.pa@code.local>
2022-09-22 10:03:05 +0400

    router added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,17 @@
-import { Box } from '@mui/material';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar />
-    <Box sx={{ pt: "58px" }}>
-    <Routes>
-    </Routes>
-    </Box>
-  </BrowserRouter>
+    <>
+      <Navbar />
+      <Box sx={{ pt: "58px" }}>
+        <Routes>
+          
+        </Routes>
+      </Box>
+    </>
   );
 }
 
