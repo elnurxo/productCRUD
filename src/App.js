a00ff6a015f587c7e0e4ff4e9e6901e372e3dc2b
elnurxo <elnurykh@code.edu.az>
2022-09-22 09:34:22 +0400

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Box } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
     <Box sx={{ pt: "58px" }}>
     <Routes>
     </Routes>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,119 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+const drawerWidth = 240;
+
+function Navbar(props) {
+
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Product CRUD
+      </Typography>
+      <Divider />
+      <List>
+        <Link to="/">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={"Home"} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to="addproduct">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={"Add Product"} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        sx={{
+          justifyContent: "center",
+          background:
+            "linear-gradient(90deg, rgba(26,36,0,1) 0%, rgba(0,87,0,1) 35%, rgba(37,80,62,1) 67%, rgba(4,93,39,1) 98%)",
+          height: "80px",
+        }}
+        component="nav"
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "block" },
+              cursor: "pointer",
+            }}
+          >
+            Product CRUD
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Link to="/">
+              <Button className="link" sx={{ color: "#fff" }}>Home</Button>
+            </Link>
+            <Link to="addsupplier">
+              <Button className="link" sx={{ color: "#fff" }}>Add Product</Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+export default Navbar;
